259ceb7038d308dff4a43c9d9e48f5ecb8c34d42
Kody Deda <kodydeda4@gmail.com>
2023-10-16 15:55:19 -0400

    init

---
+++ GuitarTuner/AppView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct AppView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  AppView()
+}
---
+++ GuitarTuner/Main.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct GuitarTunerApp: App {
+  var body: some Scene {
+    WindowGroup {
+      AppView()
+    }
+  }
+}
