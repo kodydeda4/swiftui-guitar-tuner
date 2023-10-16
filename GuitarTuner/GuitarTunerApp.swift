e17334f991b002f1ce1fa2923d2122601bec32d5
Kody Deda <kodydeda4@gmail.com>
2023-10-16 15:54:12 -0400

    Initial Commit

---
+++ GuitarTuner/GuitarTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarTunerApp.swift
+//  GuitarTuner
+//
+//  Created by Kody Deda on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GuitarTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
