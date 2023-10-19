9f92cfa96de59a9f5cdc447f26cc4bb0e264ff8c
Kody Deda <kodydeda4@gmail.com>
2023-10-19 16:01:26 -0400

    separate AppReducer from EditSettings

--- GuitarTuner/Features/AppReducer.swift
+++ GuitarTuner/Features/AppReducer.swift
@@ -1,31 +1,62 @@
 import SwiftUI
 import ComposableArchitecture
 
+// sound only works when connected to an external audio source.
 struct AppReducer: Reducer {
   struct State: Equatable {
-    @BindingState var instrument = Instrument.acoustic
-    @BindingState var tuning = InstrumentTuning.eStandard
-    @BindingState var isSheetPresented = false
+    var instrument = Instrument.acoustic
+    var tuning = InstrumentTuning.eStandard
+    @PresentationState var destination: Destination.State?
   }
-  enum Action: BindableAction, Equatable {
-    case binding(BindingAction<State>)
+  enum Action: Equatable {
     case play(Note)
+    case editSettingsButtonTapped
+    case destination(PresentationAction<Destination.Action>)
   }
   
   @Dependency(\.sound) var sound
   
   var body: some ReducerOf<Self> {
-    BindingReducer()
     Reduce { state, action in
       switch action {
-        
-      case .binding:
-        return .none
         
       case let .play(note):
         return .run { _ in
           await sound.play(note)
         }
+        
+      case .editSettingsButtonTapped:
+        state.destination = .editSettings(.init(
+          instrument: state.instrument,
+          tuning: state.tuning
+        ))
+        return .none
+        
+      case let .destination(.presented(.editSettings(.dismiss(childState)))):
+        state.instrument = childState.instrument
+        state.tuning = childState.tuning
+        return .send(.destination(.dismiss))
+        
+      default:
+        return .none
+        
+      }
+    }
+    .ifLet(\.$destination, action: /Action.destination) {
+      Destination()
+    }
+  }
+  
+  struct Destination: Reducer {
+    enum State: Equatable {
+      case editSettings(EditSettings.State)
+    }
+    enum Action: Equatable {
+      case editSettings(EditSettings.Action)
+    }
+    var body: some ReducerOf<Self> {
+      Scope(state: /State.editSettings, action: /Action.editSettings) {
+        EditSettings()
       }
     }
   }
@@ -84,12 +115,15 @@ struct AppView: View {
         .frame(maxHeight: .infinity)
         .background(Color.accentColor.gradient)
         .navigationTitle(viewStore.navigationTitle)
-        .sheet(isPresented: viewStore.$isSheetPresented) {
-          SettingsSheet(store: store)
-        }
+        .sheet(
+          store: store.scope(state: \.$destination, action: AppReducer.Action.destination),
+          state: /AppReducer.Destination.State.editSettings,
+          action: AppReducer.Destination.Action.editSettings,
+          content: EditSettingsSheet.init(store:)
+        )
         .toolbar {
           Button {
-            viewStore.send(.binding(.set(\.$isSheetPresented, true)))
+            viewStore.send(.editSettingsButtonTapped)
           } label: {
             Image(systemName: "gear")
           }
@@ -99,159 +133,11 @@ struct AppView: View {
   }
 }
 
-private struct SettingsSheet: View {
-  let store: StoreOf<AppReducer>
-  
-  var body: some View {
-    WithViewStore(store, observe: { $0 }) { viewStore in
-      NavigationStack {
-        List {
-          Header(store: store)
-          InstrumentsView(store: store)
-          TuningView(store: store)
-        }
-        .navigationTitle("Edit Settings")
-        .listStyle(.plain)
-        .toolbar {
-          Button("Done") {
-            viewStore.send(.binding(.set(\.$isSheetPresented, false)))
-          }
-        }
-      }
-    }
-  }
-}
-
-
-private struct Header: View {
-  let store: StoreOf<AppReducer>
-  
-  var body: some View {
-    WithViewStore(store, observe: { $0 }) { viewStore in
-      Section {
-        TabView(selection: viewStore.$instrument) {
-          ForEach(Instrument.allCases) { instrument in
-            Image(instrument.image)
-              .resizable()
-              .scaledToFit()
-              .padding(8)
-              .clipShape(Circle())
-              .frame(maxWidth: .infinity, alignment: .center)
-              .tag(instrument)
-          }
-        }
-        .tabViewStyle(.page(indexDisplayMode: .never))
-        .frame(maxWidth: .infinity)
-        .frame(height: 200)
-      }
-      .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
-      .background(Color.accentColor.gradient)
-      .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
-      .padding(.horizontal)
-      .listRowSeparator(.hidden)
-    }
-  }
-}
-
-private struct TuningView: View {
-  let store: StoreOf<AppReducer>
-  
-  var body: some View {
-    WithViewStore(store, observe: { $0 }) { viewStore in
-      Section {
-        ForEach(InstrumentTuning.allCases) { tuning in
-          btn(tuning)
-        }
-      } header: {
-        Text("Tuning")
-          .font(.title2)
-          .bold()
-          .foregroundStyle(.primary)
-      }
-    }
-  }
-  
-  private func btn(_ tuning: InstrumentTuning) -> some View {
-    WithViewStore(store, observe: { $0 }) { viewStore in
-      let isSelected = viewStore.tuning == tuning
-      Button {
-        viewStore.send(.binding(.set(\.$tuning, tuning)))
-      } label: {
-        HStack {
-          Image(systemName: isSelected ? "checkmark.circle.fill" : "circle")
-            .foregroundColor(isSelected ? .accentColor : .secondary)
-            .background { Color.white.opacity(isSelected ? 1 : 0) }
-            .clipShape(Circle())
-            .overlay {
-              Circle()
-                .strokeBorder()
-                .foregroundColor(.accentColor)
-                .opacity(isSelected ? 1 : 0)
-            }
-          
-          Text(tuning.description)
-        }
-        .frame(maxWidth: .infinity, alignment: .leading)
-      }
-      .buttonStyle(.plain)
-    }
-  }
-}
-
-private struct InstrumentsView: View {
-  let store: StoreOf<AppReducer>
-  
-  var body: some View {
-    WithViewStore(store, observe: { $0 }) { viewStore in
-      Section {
-        HStack {
-          ForEach(Instrument.allCases) { instrument in
-            Button {
-              viewStore.send(.binding(.set(\.$instrument, instrument)), animation: .spring())
-            } label: {
-              VStack {
-                Image(instrument.thumnailImage)
-                  .resizable()
-                  .scaledToFit()
-                  .frame(width: 75, height: 75)
-                  .frame(maxWidth: .infinity)
-                  .background(.thinMaterial)
-                  .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
-                  .overlay {
-                    RoundedRectangle(cornerRadius: 12, style: .continuous)
-                      .strokeBorder(lineWidth: 3)
-                      .foregroundColor(.accentColor)
-                      .opacity(viewStore.instrument == instrument ? 1 : 0)
-                  }
-                
-                Text(instrument.description)
-                  .font(.caption)
-                  .foregroundColor(viewStore.instrument == instrument ? .primary : .secondary)
-                  .fontWeight(.semibold)
-              }
-              .frame(maxWidth: .infinity)
-              .tag(instrument.id)
-            }
-            .buttonStyle(.plain)
-          }
-        }
-        .frame(maxWidth: .infinity)
-      } header: {
-        Text("Instrument")
-          .font(.title2)
-          .bold()
-          .foregroundStyle(.primary)
-      }
-      .listRowSeparator(.hidden, edges: .bottom)
-    }
-  }
-}
-
 // MARK: - SwiftUI Previews
 
 #Preview {
   AppView(store: Store(
-    initialState: AppReducer.State(isSheetPresented: true),
+    initialState: AppReducer.State(),
     reducer: AppReducer.init
   ))
 }
---
+++ GuitarTuner/Features/EditSettings.swift
@@ -0,0 +1,202 @@
+import SwiftUI
+import ComposableArchitecture
+
+struct EditSettings: Reducer {
+  struct State: Equatable {
+    @BindingState var instrument = Instrument.acoustic
+    @BindingState var tuning = InstrumentTuning.eStandard
+  }
+  enum Action: BindableAction, Equatable {
+    case doneButtonTapped
+    case dismiss(State)
+    case binding(BindingAction<State>)
+  }
+  @Dependency(\.dismiss) var dismiss
+  var body: some ReducerOf<Self> {
+    BindingReducer()
+    Reduce { state, action in
+      switch action {
+        
+      case .binding:
+        return .none
+        
+      case .dismiss:
+        return .none
+      
+      case .doneButtonTapped:
+        return .send(.dismiss(state))
+      }
+    }
+  }
+}
+
+private extension EditSettings.State {
+  var notes: [Note] {
+    switch instrument {
+      //    case .electric:
+      //      Array(tuning.notes)
+    case .bass:
+      Array(tuning.notes.prefix(upTo: 4))
+    default:
+      Array(tuning.notes)
+    }
+  }
+}
+
+// MARK: - SwiftUI
+
+struct EditSettingsSheet: View {
+  let store: StoreOf<EditSettings>
+  
+  var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      NavigationStack {
+        List {
+          Header(store: store)
+          InstrumentsView(store: store)
+          TuningView(store: store)
+        }
+        .navigationTitle("Edit Settings")
+        .listStyle(.plain)
+        .toolbar {
+          Button("Done") {
+            viewStore.send(.doneButtonTapped)
+          }
+        }
+      }
+    }
+  }
+}
+
+private struct Header: View {
+  let store: StoreOf<EditSettings>
+  
+  var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      Section {
+        TabView(selection: viewStore.$instrument) {
+          ForEach(Instrument.allCases) { instrument in
+            Image(instrument.image)
+              .resizable()
+              .scaledToFit()
+              .padding(8)
+              .clipShape(Circle())
+              .frame(maxWidth: .infinity, alignment: .center)
+              .tag(instrument)
+          }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(maxWidth: .infinity)
+        .frame(height: 200)
+      }
+      .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+      .background(Color.accentColor.gradient)
+      .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+      .padding(.horizontal)
+      .listRowSeparator(.hidden)
+    }
+  }
+}
+
+private struct TuningView: View {
+  let store: StoreOf<EditSettings>
+  
+  var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      Section {
+        ForEach(InstrumentTuning.allCases) { tuning in
+          btn(tuning)
+        }
+      } header: {
+        Text("Tuning")
+          .font(.title2)
+          .bold()
+          .foregroundStyle(.primary)
+      }
+    }
+  }
+  
+  private func btn(_ tuning: InstrumentTuning) -> some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      let isSelected = viewStore.tuning == tuning
+      Button {
+        viewStore.send(.binding(.set(\.$tuning, tuning)))
+      } label: {
+        HStack {
+          Image(systemName: isSelected ? "checkmark.circle.fill" : "circle")
+            .foregroundColor(isSelected ? .accentColor : .secondary)
+            .background { Color.white.opacity(isSelected ? 1 : 0) }
+            .clipShape(Circle())
+            .overlay {
+              Circle()
+                .strokeBorder()
+                .foregroundColor(.accentColor)
+                .opacity(isSelected ? 1 : 0)
+            }
+          
+          Text(tuning.description)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+      }
+      .buttonStyle(.plain)
+    }
+  }
+}
+
+private struct InstrumentsView: View {
+  let store: StoreOf<EditSettings>
+  
+  var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      Section {
+        HStack {
+          ForEach(Instrument.allCases) { instrument in
+            Button {
+              viewStore.send(.binding(.set(\.$instrument, instrument)), animation: .spring())
+            } label: {
+              VStack {
+                Image(instrument.thumnailImage)
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 75, height: 75)
+                  .frame(maxWidth: .infinity)
+                  .background(.thinMaterial)
+                  .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+                  .overlay {
+                    RoundedRectangle(cornerRadius: 12, style: .continuous)
+                      .strokeBorder(lineWidth: 3)
+                      .foregroundColor(.accentColor)
+                      .opacity(viewStore.instrument == instrument ? 1 : 0)
+                  }
+                
+                Text(instrument.description)
+                  .font(.caption)
+                  .foregroundColor(viewStore.instrument == instrument ? .primary : .secondary)
+                  .fontWeight(.semibold)
+              }
+              .frame(maxWidth: .infinity)
+              .tag(instrument.id)
+            }
+            .buttonStyle(.plain)
+          }
+        }
+        .frame(maxWidth: .infinity)
+      } header: {
+        Text("Instrument")
+          .font(.title2)
+          .bold()
+          .foregroundStyle(.primary)
+      }
+      .listRowSeparator(.hidden, edges: .bottom)
+    }
+  }
+}
+
+// MARK: - SwiftUI Previews
+
+#Preview {
+  EditSettingsSheet(store: Store(
+    initialState: EditSettings.State(),
+    reducer: EditSettings.init
+  ))
+}
