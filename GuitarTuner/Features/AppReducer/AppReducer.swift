9afc7d14d86d091c7cd21c7f016b324ebdc1ff68
Kody Deda <kodydeda4@gmail.com>
2023-11-09 17:43:20 -0500

    Update AppReducer.swift

--- GuitarTuner/Features/AppReducer/AppReducer.swift
+++ GuitarTuner/Features/AppReducer/AppReducer.swift
@@ -178,12 +178,11 @@ private extension AppReducer.State {
   var navigationTitle: String {
     instrument.description
   }
+  
   var notes: [Note] {
     switch instrument {
-      
     case .bass:
       Array(tuning.notes.prefix(upTo: 4))
-      
     default:
       Array(tuning.notes)
     }
